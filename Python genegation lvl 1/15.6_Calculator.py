bf288ba0baaf9abdf3e8749c3ecf8607aed2657e
xxxAleksandrxxx <sannywind@gmail.com>
2022-05-30 20:38:48 +0300

    BOH custom part added

--- Python genegation lvl 1/15.6_Calculator.py
+++ Python genegation lvl 1/15.6_Calculator.py
@@ -10,8 +10,43 @@ BOH
 Примечание 3. BOH = Binary, Octal, Hex.
 '''
 
+'''
 #BOH - lasy
 num = int(input())
 print(bin(num)[2:])
 print(oct(num)[2:])
 print(format(num, 'X'))
+'''
+
+'''
+#BOH - normal
+num = int(input())
+print(bin(num)[2:])
+print(oct(num)[2:])
+print(hex(num)[2:].upper())
+'''
+
+
+#BOH - custom
+def dec_to_any(num, base):
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[:base - 10]
+    num = int(num)
+    answer = ''
+    rest = base - 1
+    while num > rest:
+        count = num % base
+        if count > 9:
+            answer += alphabet[count - 10]
+        else:
+            answer += str(num % base)
+        num //= base
+    if num > 9:
+        answer += alphabet[num - 10]
+    else:
+        answer += str(num)
+    return answer[::-1]
+
+number = input()
+print(dec_to_any(number, 2))
+print(dec_to_any(number, 8))
+print(dec_to_any(number, 16))
