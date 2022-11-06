876e70d88cfda80adf8ad41499d15cc8b0d1e303
Aleksandr <sannywind@gmail.com>
2022-11-06 20:41:33 +0700

    add part 8 to topic 8

--- Python generation lvl 2/Stepik_lvl2_08 - set.py
+++ Python generation lvl 2/Stepik_lvl2_08 - set.py
@@ -521,3 +521,37 @@ s3 = set([int(i) for i in input().split()])
 
 print(*sorted(s-s1-s2-s3))
 '''
+
+#%%
+# 8.8
+
+# 8.8.4
+'''
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+s = {word[0].lower() for word in words}
+print(*sorted(s))
+'''
+
+
+# 8.8.5
+
+#sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+
+#s = {word.strip(',.:;()').lower() for word in sentence.split()}
+#print(*sorted(s))
+
+
+
+# 8.8.6
+
+#sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+#s = {w for w in [word.strip(',.:;()').lower() for word in sentence.split()] if len(w)<4}
+#print(*sorted(s))
+
+
+# 8.8.7
+'''
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+s = {file.lower() for file in files if file.lower().endswith('.png')}
+print(*sorted(s))
+'''
