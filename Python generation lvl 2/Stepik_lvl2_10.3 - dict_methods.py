ead41d26e57b7da0f5b61cbb0d443776d7469503
Aleksandr <sannywind@gmail.com>
2022-11-14 18:50:48 +0700

    add task 10.3.14

--- Python generation lvl 2/Stepik_lvl2_10.3 - dict_methods.py
+++ Python generation lvl 2/Stepik_lvl2_10.3 - dict_methods.py
@@ -90,3 +90,126 @@ result = {sym : text.count(sym) for sym in set(text)}
 print(len(result))
 print(result)
 '''
+
+
+# 10.3.13
+'''
+Дополните приведенный код, чтобы он вывел наиболее часто встречающееся слово строки s. Если таких слов несколько, должно быть выведено то, что меньше в лексикографическом порядке.
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+'''
+# v1
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+'''
+#s = 'a a a a a b b b b b c c c d d s r r r r r g'
+s_list = s.split()
+s_count = {word : s_list.count(word) for word in set(s_list)}
+# print(s_count)
+name = list()
+count = list()
+for elem in s_count:
+    name.append(elem)
+    count.append(s_count[elem])
+ # сортируем два списка по возрастанию количества
+for i in range(len(name)-1):
+    for j in range(len(name)-i-1):
+        if count[j] > count[j+1]:
+            count[j], count[j+1] = count[j+1], count[j]
+            name[j], name[j+1] = name[j+1], name[j]
+# print(count)
+# print(name)
+repeat = count.count(count[-1])
+# print(repeat)
+# сортируем самые большое количество по имени
+if count.count(count[-1]) > 1:
+    # print('YES')
+    for i in range(len(name) - repeat, len(name)-1):
+        # print('i', i)
+        # print('start', len(name) - repeat)
+        # print('end', len(name)-1)
+        # print('len(name)', len(name))
+        for j in range(len(name) - repeat, len(name)-1):
+            # print('j', j)
+            if name[j] < name[j+1]:
+                # print(f'name[{j}]', name[j])
+                # print(f'name[{j+1}]', name[j+1])
+                count[j], count[j+1] = count[j+1], count[j]
+                name[j], name[j+1] = name[j+1], name[j]
+#print(count)
+print(name[-1])
+'''
+'''
+# v2
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+
+s_list = s.split()
+s_dict = {elem : s_list.count(elem) for elem in set(s_list)}
+max_el_count = 0
+max_el_name = 'z'
+for elem in s_dict:
+    if s_dict[elem] > max_el_count:
+        max_el_count = s_dict[elem]
+        max_el_name = elem
+    elif s_dict[elem] == max_el_count:
+        if elem < max_el_name:
+            max_el_name = elem
+print(max_el_name)    
+'''
+'''
+# v3
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+s_dict = {}
+for word in s.split():
+    s_dict[word] = s_dict.get(word, 0) + 1
+max_el_count = max(s_dict.values())
+answer = list()
+
+for key, value in s_dict.items():
+    if value == max_el_count:
+        answer.append(key)
+print(min(answer))
+'''
+
+
+# 10.3.14
+'''
+Вам доступен список pets, содержащий информацию о собаках и их владельцах.  Каждый элемент списка – это кортеж вида (кличка собаки, имя владельца, фамилия владельца, возраст владельца).
+Дополните приведенный код так, чтобы в переменной result хранился словарь, в котором для каждого владельца будут перечислены его собаки. Ключом словаря должен быть кортеж (имя, фамилия, возраст владельца), а значением – список кличек собак (сохранив исходный порядок следования).
+
+Примечание 1. Не забывайте: кортежи являются неизменяемыми, поэтому могут быть ключами словаря.
+Примечание 2. Обратите внимание, что у некоторых владельцев по несколько собак.
+Примечание 3. Выводить содержимое словаря result не нужно.
+'''
+'''
+pets = [('Hatiko', 'Parker', 'Wilson', 50),
+        ('Rusty', 'Josh', 'King', 25),
+        ('Fido', 'John', 'Smith', 28),
+        ('Butch', 'Jake', 'Smirnoff', 18),
+        ('Odi', 'Emma', 'Wright', 18),
+        ('Balto', 'Josh', 'King', 25),
+        ('Barry', 'Josh', 'King', 25),
+        ('Snape', 'Hannah', 'Taylor', 40),
+        ('Horry', 'Martha', 'Robinson', 73),
+        ('Giro', 'Alex', 'Martinez', 65),
+        ('Zooma', 'Simon', 'Nevel', 32),
+        ('Lassie', 'Josh', 'King', 25),
+        ('Chase', 'Martha', 'Robinson', 73),
+        ('Ace', 'Martha', 'Williams', 38),
+        ('Rocky', 'Simon', 'Nevel', 32)]
+result = {}
+'''
+'''
+# v1
+for tup in pets:
+    if tup[1:] not in result:
+        result[tup[1:]] = []
+    result[tup[1:]].append(*tup[:1])
+
+print(result)
+'''
+'''
+# v2
+for tup in pets:
+    result.setdefault(tup[1:], []).append(tup[0])
+
+print(result)
+'''
