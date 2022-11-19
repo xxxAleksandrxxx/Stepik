775c0621606da0f2986f5e6da0d1172461591d28
Aleksandr <sannywind@gmail.com>
2022-11-19 18:08:41 +0700

    add task 10.5.9

--- Python generation lvl 2/Stepik_lvl2_10.3 - dict_methods.py
+++ Python generation lvl 2/Stepik_lvl2_10.3 - dict_methods.py
@@ -595,6 +595,25 @@ favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebec
 
 result = {}
 '''
+'''
 favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62, 'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654, 'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271, 'anna': 55, 'madlen': 876}
 
 result = {key: value for key, value in favorite_numbers.items() if 9<value<100}
+'''
+
+
+# 10.5.9
+'''
+Дополните приведенный код, используя генератор, так, чтобы получить словарь result, состоящий из всех элементов словаря months , в которых ключ и значение поменялись местами.
+months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+result = {}
+'''
+'''
+months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+result = {value: key for key, value in months.items()}
+'''
+
+
+
