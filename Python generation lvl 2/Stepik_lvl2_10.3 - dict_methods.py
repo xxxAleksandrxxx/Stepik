32ca955a71e790f32d38a819920bb3696d550bc3
Aleksandr <sannywind@gmail.com>
2022-11-19 17:58:42 +0700

    add task 10.5.7

--- Python generation lvl 2/Stepik_lvl2_10.3 - dict_methods.py
+++ Python generation lvl 2/Stepik_lvl2_10.3 - dict_methods.py
@@ -572,3 +572,19 @@ numbers = [34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95, 1, 36, -38, -19, 1, 6,
 
 result = {i: numbers[i]**2 for i in range(len(numbers))}
 '''
+
+
+# 10.5.7
+'''
+Дополните приведенный код, используя генератор, чтобы получить словарь result, состоящий из всех элементов словаря colors, кроме тех, у которых значением является None.
+colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange', 'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber', 'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None, 'c22': 'Sand', 'c23': None}
+
+result = {}
+'''
+'''
+colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange', 'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber', 'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None, 'c22': 'Sand', 'c23': None}
+
+result = {key: value for key, value in colors.items() if value != None}
+'''
+
+
