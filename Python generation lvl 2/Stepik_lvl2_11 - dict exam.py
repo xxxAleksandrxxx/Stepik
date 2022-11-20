c1f5c611c7a439c37543b7674d32cd5908ec728c
Aleksandr <sannywind@gmail.com>
2022-11-20 20:37:50 +0700

    add строка web-запроса из словаря

--- Python generation lvl 2/Stepik_lvl2_11 - dict exam.py
+++ Python generation lvl 2/Stepik_lvl2_11 - dict exam.py
@@ -193,3 +193,40 @@ word_count_dict = word_count_dict | dict.fromkeys(('Q', 'Z'), 10)
 
 print(sum([word_count_dict[elem] for elem in input()]))
 '''
+
+
+# 11.2.4 Строка запроса
+'''
+Строка запроса (query string) — часть URL адреса, содержащая ключи и их значения. Она начинается после вопросительного знака и идет до конца адреса. Например:
+
+https://beegeek.ru?name=timur     # строка запроса: name=timur
+Если параметров в строке запроса несколько, то они отделяются символом амперсанда &:
+
+https://beegeek.ru?name=timur&color=green     # строка запроса: name=timur&color=green 
+Напишите функцию build_query_string(), которая принимает на вход словарь с параметрами и возвращает строку запроса, сформированную из этих параметров.
+
+Примечание 1. В итоговой строке параметры должны быть отсортированы в лексикографическом порядке ключей словаря.
+
+Примечание 2. Следующий программный код:
+
+print(build_query_string({'name': 'timur', 'age': 28}))
+print(build_query_string({'sport': 'hockey', 'game': 2, 'time': 17}))
+должен выводить:
+
+age=28&name=timur
+game=2&sport=hockey&time=17
+Примечание 3. Вызывать функцию build_query_string() не нужно, требуется только реализовать. 
+'''
+'''
+def build_query_string(my_dict):
+    answer = []
+    for key, value in my_dict.items():
+        answer.append(f'{key}={value}')
+    answer.sort()
+    answer = '&'.join(answer)
+    return answer
+
+print(build_query_string({'name': 'timur', 'age': 28}))
+print(build_query_string({'sport': 'hockey', 'game': 2, 'time': 17}))
+'''
+
