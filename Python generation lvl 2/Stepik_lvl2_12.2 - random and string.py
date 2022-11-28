35c56f99cf17f1b85c046f475c6525ae521a864e
Aleksandr <sannywind@gmail.com>
2022-11-28 21:50:12 +0700

    add task 12.2.11 - random card for Bingo game

--- Python generation lvl 2/Stepik_lvl2_12.2 - random and string.py
+++ Python generation lvl 2/Stepik_lvl2_12.2 - random and string.py
@@ -251,3 +251,44 @@ from random import sample
 word = input()
 print(''.join(sample(word, len(word))))
 '''
+
+#%%
+# 12.2.11 - Бинго
+'''
+ля игры в бинго требуется карточка размером 5×5, содержащая различные (уникальные) целые числа от 1 до 75 (включительно), при этом центральная клетка является пустой (она заполняется числом 0).
+
+Напишите программу, которая с помощью модуля random генерирует и выводит случайную карточку для игры в бинго.
+
+Примечание 1. Для наглядности рекомендуем отводить на вывод каждого числа ровно 3 символа. Для этого используйте строковый метод ljust().
+
+Примечание 2. Пример возможного ответа:
+
+1  16 31 46 61
+10 30 42 47 68
+3  18 0  48 63
+9  19 34 49 70
+5  20 35 50 65
+Возможны и другие способы генерации карточки для игры в бинго.
+'''
+'''
+# v1
+from random import sample
+num_list = sample(range(1,76), 25)
+print(num_list)
+bingo_ticket = [[num_list[i*5 + j] for j in range(5)] for i in range(5)]
+bingo_ticket[2][2]=0
+for row in bingo_ticket:
+    for elem in row:
+        print(str(elem).rjust(3, ' '), end ='')
+    print()
+'''
+'''
+# v2
+from random import sample
+num_list = sample(range(1,76), 25)
+ticket = [num_list[i:i+5] for i in range(0, 21, 5)]
+for row in ticket:
+    for elem in row:
+        print(str(elem).rjust(3, ' '), end = '')
+    print()
+'''
