2d1e197db4c9fc15cd7aef26ad224039b3ba2b9e
Aleksandr <sannywind@gmail.com>
2022-12-01 21:07:43 +0800

    add task 12.3.8 - calculate pi

--- Python generation lvl 2/Stepik_lvl2_12.3 - monte-karlo.py
+++ Python generation lvl 2/Stepik_lvl2_12.3 - monte-karlo.py
@@ -40,3 +40,34 @@ for _ in range(n):
 print(s0*k/n)
 '''
 
+# 12.3.8 расчитать число pi
+'''
+Напишите программу, которая при помощи метода Монте-Карло определяет приближённое значение числа π.
+y^2 + x^2 = 1^2
+-1 <= x <= 1
+-1 <= y <= 1
+'''
+'''
+import matplotlib.pyplot as plt
+from random import uniform
+
+n = 10**4
+k = 0
+s0 = 4
+x_in, x_out, y_in, y_out = list(), list(), list(), list()
+for _ in range(n):
+    x = uniform(-1, 1)
+    y = uniform(-1, 1)
+    if x**2 + y**2 <= 1:
+        x_in.append(x)
+        y_in.append(y)
+        k += 1
+    else:
+        x_out.append(x)
+        y_out.append(y)
+plt.figure(figsize = (6, 6), dpi=80)
+plt.scatter(x_in, y_in, s=1, c='b')
+plt.scatter(x_out, y_out, s=1, c='r')
+print('k =',k)
+print(f'pi = 4 * {k}/{n} = {4 * k/(n)}')
+'''
