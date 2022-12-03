8560cfebecb3def85e1318801beaeb153dc28a78
Aleksandr <sannywind@gmail.com>
2022-12-03 23:29:23 +0800

    add task 13.1.10

---
+++ Python generation lvl 2/Stepik_lvl2_13.1 - Decimal.py
@@ -0,0 +1,84 @@
+#%%
+if 0.1+0.1+0.1 == 0.3:
+    print('YES')
+else:
+    print('NO')
+
+#%%
+import decimal
+
+a = 10
+b = decimal.Decimal(a)
+print(a, type(a))
+print(b, type(b))
+ 
+#%%
+import decimal
+
+a = 0.1
+b = decimal.Decimal(a)
+print(type(a), a, sep='\n')
+print()
+print(type(b), b, sep='\n')
+
+#%%
+from decimal import Decimal
+a = '123.456789000'
+b = Decimal(a)
+print(a, type(a))
+print(b, type(b))
+
+#%%
+# встроенные функции, выводящие Decimal
+from decimal import Decimal
+
+a = Decimal('12.2')
+
+print(a, type(a))
+print(a.exp(), type(a.exp()))
+print(a.sqrt(), type(a.sqrt()))
+print(a.ln(), type(a.ln()))
+print(a.log10(), type(a.log10()))
+
+#%%
+# а сколько знаков после запятой?
+b = a.sqrt()
+print()
+print('b', b)
+print(len(str(b).split('.')[1]))
+
+#%%
+# детализация вывода - as_tuple()
+import decimal
+
+a = decimal.Decimal('123.05')
+b = a.as_tuple()
+print(a, type(a))
+print(b, type(b))
+print('sign:    ', type(b.sign), b.sign)
+print('digits:  ', type(b.digits), b.digits)
+print('exponent:', type(b.exponent), b.exponent)
+
+#%%
+# Работа с контекстом decimal
+
+import decimal
+
+decimal.getcontext()
+
+a = decimal.Decimal('31.1428')
+
+decimal.getcontext().prec = 3
+print(a)
+print(a*1)
+
+#%%
+# 13.1.10
+'''
+Decimal числа, разделенные символом пробела, хранятся в строковой переменной s. Дополните приведенный код, чтобы он вывел сумму наибольшего и наименьшего Decimal числа.
+s = '0.77 4.03 9.06 3.80 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 1.56 6.67 1.46 5.26 4.83 7.23 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 5.78 5.37 0.03 9.60 8.86 2.73 5.83 6.50'
+'''
+from decimal import Decimal
+s = '0.77 4.03 9.06 3.80 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 1.56 6.67 1.46 5.26 4.83 7.23 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 5.78 5.37 0.03 9.60 8.86 2.73 5.83 6.50'
+s_dec = [Decimal(number) for number in s.split()]
+print(min(s_dec)+max(s_dec))
