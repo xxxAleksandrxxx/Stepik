0a6b9ab28aa2ab530c9c1b3653866c12be193697
Aleksandr <sannywind@gmail.com>
2022-12-15 21:20:47 +0700

    add task 14.2.2 - dot line

---
+++ Python generation lvl 2/Stepik_lvl2_14.2 - turtle.py
@@ -0,0 +1,44 @@
+# 14.2
+'''
+import turtle
+
+t1 = turtle.Turtle()
+t1.speed(0)
+t1.pencolor('DarkOrange')
+def paint():
+    for i in range(1, 5):
+        t1.pensize(i)
+        t1.forward(100)
+        t1.left(90)
+
+for i in range(10):
+    paint()
+    t1.left(10)
+turtle.exitonclick()
+'''
+'''
+import turtle
+
+turtle.colormode(255)
+turtle.Screen().bgcolor(0, 0, 255)
+
+turtle.exitonclick()
+'''
+
+
+# 14.2.2
+'''
+Напишите программу, которая рисует пунктирную линию
+'''
+'''
+import turtle
+
+turtle.speed(9)
+l = 10
+line = 100
+for i in range(0, line, l):
+    turtle.dot()
+    turtle.up()
+    turtle.fd(l)
+turtle.exitonclick()
+'''
