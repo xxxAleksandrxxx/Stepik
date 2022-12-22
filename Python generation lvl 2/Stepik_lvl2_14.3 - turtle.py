6b82705fc6e802d8cbc56349728c93d9a97271de
Aleksandr <sannywind@gmail.com>
2022-12-22 19:35:01 +0700

    add task 14.3.3

--- Python generation lvl 2/Stepik_lvl2_14.3 - turtle.py
+++ Python generation lvl 2/Stepik_lvl2_14.3 - turtle.py
@@ -34,6 +34,7 @@ turtle.exitonclick()
 '''
 Напишите программу, которая рисует изображение домика по образцу.
 '''
+'''
 import turtle
 
 # turtle.tracer(0, 0)
@@ -77,5 +78,50 @@ turtle.down()
 tri(b, h, 0, h+h/2)
 
 turtle.update()
+
+turtle.exitonclick()
+'''
+
+
+# 14.3.3
+'''
+Напишите программу, которая рисует изображение светофора
+'''
+
+import turtle
+t = turtle.Turtle()
+t.hideturtle()
+t.speed(0)
+size = 200
+h = size
+l = size*0.4
+r = 0.7*l/2
+
+def circle(r0, x0=0, y0=0, color0='red'):
+    t.up()
+    t.goto(x0, y0)
+    t.down()
+    t.fillcolor(color0)
+    t.begin_fill()
+    t.circle(r0)
+    t.end_fill()
+
+X0 = 0
+Y0 = h/2
+t.up()
+t.goto(X0, Y0)
+t.down()
+t.fillcolor('black')
+t.begin_fill()
+t.goto(l/2+X0, Y0)
+t.goto(l/2+X0, Y0-h)
+t.goto(-l/2+X0, Y0-h)
+t.goto(-l/2+X0, Y0)
+t.goto(X0, Y0)
+t.end_fill()
+Y0 -= h/2+r
+circle(r, X0, Y0+h/2*0.62, 'red')
+circle(r, X0, Y0+0, 'yellow')
+circle(r, X0, Y0-h/2*0.62, 'green')
 
 turtle.exitonclick()
