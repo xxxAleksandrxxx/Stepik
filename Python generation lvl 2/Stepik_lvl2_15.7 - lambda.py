45bb360be1d8fc17fbdfb54ea1d7d1b1d6f5e804
Aleksandr <sannywind@gmail.com>
2023-01-02 21:51:39 +0700

    add task 15.7.16 - step by step

---
+++ Python generation lvl 2/Stepik_lvl2_15.7 - lambda.py
@@ -0,0 +1,51 @@
+#%% 15.7.15
+
+from functools import reduce 
+
+floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+numbers = [4, 6, 9, 23, 5]
+
+# Исправьте этот код
+map_result = list(map(lambda num: round(num**2, 1), floats))
+filter_result = list(filter(lambda name: name==name[::-1] and len(name)>4, words))
+reduce_result = reduce(lambda num1, num2: num1*num2, numbers, 1)
+
+print(map_result)
+print(filter_result)
+print(reduce_result)
+
+#%% 15.7.16
+from functools import reduce
+
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+# отбираем 10М и 'primary'
+answer = filter(lambda elem: elem[1]>10000000 and 'primary' in elem[2], data)
+# формируем список с городами
+answer = reduce(lambda a, b: a + [b[0]], answer, list())
+# сортируем список
+answer = sorted(answer)
+print('Cities: ', end='')  
+print(*answer, sep=', ')
+
+print()
+# print(sorted(reduce(lambda a, b: [a] + [b], filter(lambda elem: elem if elem[1]>10000 and 'primary' in elem[2], data))))
