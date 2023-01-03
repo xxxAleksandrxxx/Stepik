673b97925d34eec3dbb645425bf1fcdf2f50e6ad
Aleksandr <sannywind@gmail.com>
2023-01-03 22:15:38 +0700

    add task 15.8.8 v1

--- Python generation lvl 2/Stepik_lvl2_15.8 - lambda.py
+++ Python generation lvl 2/Stepik_lvl2_15.8 - lambda.py
@@ -99,3 +99,39 @@ True
 False
 False
 True''')
+
+#%% 15.8.8 v1
+"""Напишите функцию is_num, используя синтаксис анонимных функций, которая принимает строковый аргумент и возвращает значение True, если переданный аргумент является числом (целым или вещественным) и False в противном случае.
+"""
+def isfloat(my_num):
+    try: 
+        float(my_num)
+        return True
+    except:
+        return False
+
+is_num = lambda num: isfloat(num)
+
+print(is_num('10.34ab'))
+print(is_num('10.45'))
+print(is_num('-18'))
+print(is_num('-34.67'))
+print(is_num('987'))
+print(is_num('abcd'))
+print(is_num('123.122.12'))
+print(is_num('-123.122'))
+print(is_num('--13.2'))
+print(is_num('1-1'))
+print(is_num('0'))
+print()
+print('''False
+True
+True
+True
+True
+False
+False
+True
+False
+False
+True''')
