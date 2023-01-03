a117bdbe7f154a4e10983f12cb8b15e0e9a86643
Aleksandr <sannywind@gmail.com>
2023-01-03 17:49:37 +0700

    add task 15.8.6 v1

--- Python generation lvl 2/Stepik_lvl2_15.8 - lambda.py
+++ Python generation lvl 2/Stepik_lvl2_15.8 - lambda.py
@@ -11,3 +11,26 @@ func = lambda int_num: int_num%19 == 0 or int_num%13 == 0
 # print(func(15))
 # print(func(247))
 '''
+
+#%% 15.8.6 v1
+
+"""
+Напишите функцию func, используя синтаксис анонимных функций, которая принимает строковый аргумент и возвращает значение True, если переданный аргумент начинается и заканчивается на английскую букву a (регистр буквы неважен) и False в противном случае.
+"""
+func = lambda letter: letter[0].lower()=='a' and letter[-1].lower()=='a'
+
+
+print(func('abcd'))
+print(func('bcda'))
+print(func('abcda'))
+print(func('Abcd'))
+print(func('bcdA'))
+print(func('abcdA'))
+print()
+answer = '''False
+False
+True
+False
+False
+True'''
+print(answer)
