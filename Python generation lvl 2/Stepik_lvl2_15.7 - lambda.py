194174eb486c24aedbf02e7eb4a4d16eeb5be1e7
Aleksandr <sannywind@gmail.com>
2023-01-03 17:26:09 +0700

    add task 15.7.16 v2

--- Python generation lvl 2/Stepik_lvl2_15.7 - lambda.py
+++ Python generation lvl 2/Stepik_lvl2_15.7 - lambda.py
@@ -15,7 +15,7 @@ print(map_result)
 print(filter_result)
 print(reduce_result)
 
-#%% 15.7.16
+#%% 15.7.16 v1
 from functools import reduce
 
 data = [['Tokyo', 35676000, 'primary'],
@@ -48,4 +48,29 @@ print('Cities: ', end='')
 print(*answer, sep=', ')
 
 print()
-# print(sorted(reduce(lambda a, b: [a] + [b], filter(lambda elem: elem if elem[1]>10000 and 'primary' in elem[2], data))))
+#%% 15.7.16 v2
+from functools import reduce
+
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+
+print('Cities: ', end='')  
+print(*sorted(reduce(lambda a, b: a + [b[0]], filter(lambda elem: elem[1]>10000000 and 'primary' in elem[2], data), list())), sep=', ')
