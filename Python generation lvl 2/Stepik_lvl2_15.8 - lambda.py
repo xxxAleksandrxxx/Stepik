ecb698b4773df0ac00bb62cbe80fbff39b6ee847
Aleksandr <sannywind@gmail.com>
2023-01-03 21:39:06 +0700

    add task 15.8.7 v2

--- Python generation lvl 2/Stepik_lvl2_15.8 - lambda.py
+++ Python generation lvl 2/Stepik_lvl2_15.8 - lambda.py
@@ -77,3 +77,25 @@ True
 False
 False
 True''')
+
+#%% 15.8.7. v2
+# string.replace(old, new, count)
+is_non_negative_num = lambda x: x.replace('.', '', 1).isdigit() 
+
+print(is_non_negative_num('10.34ab'))
+print(is_non_negative_num('10.45'))
+print(is_non_negative_num('-18'))
+print(is_non_negative_num('-34.67'))
+print(is_non_negative_num('987'))
+print(is_non_negative_num('abcd'))
+print(is_non_negative_num('123.122.12'))
+print(is_non_negative_num('123.122'))
+print()
+print('''False
+True
+False
+False
+True
+False
+False
+True''')
