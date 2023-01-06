4e5181fe8afb51e3224cba8f2b66f7ac7b77e987
Aleksandr <sannywind@gmail.com>
2023-01-06 15:53:19 +0700

    add task 15.9.8

--- Python generation lvl 2/Stepik_lvl2_15.9 - any all zip enumerate.py
+++ Python generation lvl 2/Stepik_lvl2_15.9 - any all zip enumerate.py
@@ -35,3 +35,67 @@ c = ['apple', 'banana', 'carrot']
 z = zip(a, b, c)
 print(type(z))
 print(*z)
+#%% zip - формируем словарь
+name = ['Timur', 'Ruslan', 'Rustam']
+age = [28, 21, 19]
+my_dict = dict(zip(name, age))
+
+print(my_dict)
+for k, v in my_dict.items():
+    print(k, v)
+
+#%% zip - параллельный перебор по нескольким коллекциям
+name = ['Timur', 'Ruslan', 'Rustam']
+age = [28, 21, 19]
+color = ('red', 'white', 'yellow', 'black')
+for n, a, c in zip(name, age, color):
+    print(n, a, c)
+
+#%%
+from itertools import zip_longest
+name = ['Timur', 'Ruslan', 'Rustam']
+age = [28, 21, 19]
+color = ('red', 'white', 'yellow', 'black')
+for n, a, c in zip_longest(name, age, color, fillvalue=0):
+    print(n, a, c)
+
+#%%
+name = ['Timur', 'Ruslan', 'Rustam']
+age = [28, 21, 19]
+f = zip(name, age)
+print(*f)
+print(*f)
+for i in range(2):
+    print(f'step {i}')
+    for elem in f:
+        print(elem)
+    print()
+
+#%% 15.9.8
+
+"""Функция ignore_command() принимает на вход один строковый аргумент command – команда, которую нужно проверить,
+
+и возвращает значение True, если в команде содержится подстрока из списка ignore и False – если нет.
+def ignore_command(command):
+    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+
+    for word in ignore:
+        if word in command:
+            return True
+    return False
+Перепишите функцию ignore_command(), чтобы она использовала встроенные функции all()/any() сохранив при этом ее функционал."""
+
+test = (
+    ('get ip', True),
+    ('select all', True),
+    ('delete', True),
+    ('trancate', False)
+)
+
+def ignore_command(command):
+    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+    return any(map(lambda x: x in command, ignore))
+
+for q, a in test:
+    print('answer:', a)
+    print(ignore_command(q))
