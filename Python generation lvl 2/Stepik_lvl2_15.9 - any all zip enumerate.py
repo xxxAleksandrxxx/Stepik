794eca2c24d7bba648a0dcc525261df52231b167
Aleksandr <sannywind@gmail.com>
2023-01-06 22:00:33 +0700

    add task 15.9.11 v2

--- Python generation lvl 2/Stepik_lvl2_15.9 - any all zip enumerate.py
+++ Python generation lvl 2/Stepik_lvl2_15.9 - any all zip enumerate.py
@@ -215,3 +215,7 @@ print('v2', *map(lambda x, y, z: f'{x}-{y}-{z}', data_x))
 # 15.9.11 v1
 abscissas, ordinates, applicates = [[float(num) for num in input().split()] for _ in range(3)]
 print(all(map(lambda x: x[0]**2 + x[1]**2 + x[2]**2 <= 4, zip(abscissas, ordinates, applicates))))
+
+# 15.9.11 v2
+abscissas, ordinates, applicates = [[float(num) for num in input().split()] for _ in range(3)]
+print(all(x**2 + y**2 + z**2 <= 4 for x, y, z in zip(abscissas, ordinates, applicates)))
