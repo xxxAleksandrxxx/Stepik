0754e70c70d7ae99ac4277824575820fff00f417
Aleksandr <sannywind@gmail.com>
2023-01-08 20:50:30 +0700

    add task 17.2.12

--- Python generation lvl 2/Stepik_lvl2_17 - files.py
+++ Python generation lvl 2/Stepik_lvl2_17 - files.py
@@ -9,3 +9,16 @@ f_content = open(input(), 'r')
 for raw in f_content:
     print(raw.strip())
 f_content.close()
+
+#%% 17.2.12 - Предпоследняя строка
+"""На вход программе подается строка с именем текстового файла. Напишите программу, которая выводит на экран его предпоследнюю строку."""
+
+# v1
+f_content = open(input(), 'r')
+print(f_content.readlines()[-2].strip())
+f_content.close()
+
+# v2
+f_content = open(input(), 'r', encoding='utf-8')
+print(list(f_content)[-2].rstrip())
+f_content.close()
