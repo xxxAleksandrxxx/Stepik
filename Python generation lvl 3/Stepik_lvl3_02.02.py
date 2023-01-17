56cb52971134f3c8c54607347b951423f76da6bb
Aleksandr <sannywind@gmail.com>
2023-01-17 22:36:29 +0700

    add task 2.11 v2

--- Python generation lvl 3/Stepik_lvl3_02.02.py
+++ Python generation lvl 3/Stepik_lvl3_02.02.py
@@ -299,3 +299,17 @@ print(index_of_nearest([], 17))
 print(index_of_nearest([7, 13, 3, 5, 18], 0))
 print(index_of_nearest([9, 5, 3, 2, 11], 4))
 print(index_of_nearest([7, 5, 4, 4, 3], 4))
+
+
+#%% 2.11 v2
+def index_of_nearest(numbers, number):
+    if numbers:
+        answer = min(numbers, key=lambda x: abs(x - number))
+        return numbers.index(answer)
+    else:
+        return -1
+
+print(index_of_nearest([], 17))
+print(index_of_nearest([7, 13, 3, 5, 18], 0))
+print(index_of_nearest([9, 5, 3, 2, 11], 4))
+print(index_of_nearest([7, 5, 4, 4, 3], 4))
