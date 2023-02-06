7289603a4b5132224751c2a301a990509a8d3311
Aleksandr <sannywind@gmail.com>
2023-02-06 16:05:10 +0700

    add task 3.1.12

--- Python generation lvl 3/Stepik_lvl3_03.01.py
+++ Python generation lvl 3/Stepik_lvl3_03.01.py
@@ -105,3 +105,29 @@ with ZipFile(file_name, 'r') as z:
     z.printdir()
     with z.open('1.clue', 'r') as f:
         print(f.read().decode('utf-8'))
+
+
+
+#%% 3.1.12 - Функция saturdays_between_two_dates()
+
+"""Реализуйте функцию saturdays_between_two_dates(), которая принимает два аргумента в следующем порядке:
+
+start — начальная дата, тип date
+end — конечная дата, тип date
+Функция должна возвращать количество суббот между датами start и end включительно.
+
+Примечание 1. Даты могут передаваться в любом порядке, то есть не гарантируется, что первая дата меньше второй.
+
+Примечание 2. В тестирующую систему сдайте программу, содержащую только необходимую функцию saturdays_between_two_dates(), но не код, вызывающий ее."""
+
+from datetime import date
+
+def saturdays_between_two_dates(start, end):
+    start, end = sorted((start.toordinal(), end.toordinal()))
+    answer = abs(start - end)//7
+    for d in range(end - abs(start - end)%7, end + 1):
+        if date.fromordinal(d).isoweekday() == 6:
+            answer += 1
+    return answer
+
+
