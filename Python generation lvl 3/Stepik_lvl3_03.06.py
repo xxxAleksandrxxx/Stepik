4804e1634913a2827a17da97c0b495bdec1ebfac
Aleksandr <sannywind@gmail.com>
2024-04-06 15:19:29 +0700

    add task 3.6.11

--- Python generation lvl 3/Stepik_lvl3_03.06.py
+++ Python generation lvl 3/Stepik_lvl3_03.06.py
@@ -41,3 +41,31 @@
 #     print(calculate_it(my_sum, 1, 2, 3, 4, 5))
 
 
+
+##########################################
+# 3.6.11
+# Функция get_the_fastest_func()
+# Реализуйте функцию get_the_fastest_func(), которая принимает два аргумента в следующем порядке:
+# funcs — список произвольных функций
+# arg — произвольный объект
+# Функция get_the_fastest_func() должна возвращать функцию из списка funcs, которая затратила на вычисление значения при вызове с аргументом arg наименьшее количество времени.
+# Примечание. В тестирующую систему сдайте программу, содержащую только необходимую функцию get_the_fastest_func(), но не код, вызывающий ее.
+
+import time
+
+def get_the_fastest_func(funcs, arg):
+    '''
+    calculates execution time of function from funcs list
+    finds the fastest one
+    returns fastest function
+    '''
+    time_fastest = float('inf')
+    for func in funcs:
+        time_start = time.monotonic()
+        func(arg)
+        time_end = time.monotonic()
+        time_execution = time_end - time_start
+        if time_execution < time_fastest:
+            time_fastest = time_execution
+            func_fastest = func
+    return func_fastest
