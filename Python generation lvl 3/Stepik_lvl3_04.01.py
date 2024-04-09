078e883eea77a1f6925888b430ce75c099af2c5a
Aleksandr <sannywind@gmail.com>
2024-04-09 20:54:00 +0700

    add task 4.1.18

--- Python generation lvl 3/Stepik_lvl3_04.01.py
+++ Python generation lvl 3/Stepik_lvl3_04.01.py
@@ -456,106 +456,123 @@
 # DESC, если даты в введенной последовательности расположены строго в порядке убывания
 # MIX, если даты в введенной последовательности расположены ни в порядке возрастания, ни в порядке убывания
 
-import sys
+# import sys
 
-# my solution -  оказалось самым лучим! 
-def check_order1(the_dates=sys.stdin):
-    '''
-    input mostly from stdin but cold be a list of strings
-    date format DD.MM.YYYY
-    '''
-    # print("Func1")
-    flag_increase = True
-    flag_decrease = True
-    if type(the_dates) == list:
-        date_0 = the_dates[0]
-        the_dates = the_dates[1:]
-    else:
-        date_0 = the_dates.readline()
-    date_0 = date_0[6:10] + date_0[2:6] + date_0[:2]# YYYY.MM.DD
-    for elem in the_dates:
-        date_1 = elem[6:10] + elem[2:6] + elem[:2]  # YYYY.MM.DD
-        if date_1 > date_0:
-            flag_decrease = False
-        elif date_1 < date_0:
-            flag_increase = False
-        elif date_1 == date_0:
-            flag_increase = False
-            flag_decrease = False
-            break
-        date_0 = date_1
-    if flag_increase == True and flag_decrease == False:
-        return "ASC"
-    elif flag_increase == False and flag_decrease == True:
-        return "DESC"
-    else:
-        return "MIX"
+# # my solution -  оказалось самым лучим! 
+# def check_order1(the_dates=sys.stdin):
+#     '''
+#     input mostly from stdin but cold be a list of strings
+#     date format DD.MM.YYYY
+#     '''
+#     # print("Func1")
+#     flag_increase = True
+#     flag_decrease = True
+#     if type(the_dates) == list:
+#         date_0 = the_dates[0]
+#         the_dates = the_dates[1:]
+#     else:
+#         date_0 = the_dates.readline()
+#     date_0 = date_0[6:10] + date_0[2:6] + date_0[:2]# YYYY.MM.DD
+#     for elem in the_dates:
+#         date_1 = elem[6:10] + elem[2:6] + elem[:2]  # YYYY.MM.DD
+#         if date_1 > date_0:
+#             flag_decrease = False
+#         elif date_1 < date_0:
+#             flag_increase = False
+#         elif date_1 == date_0:
+#             flag_increase = False
+#             flag_decrease = False
+#             break
+#         date_0 = date_1
+#     if flag_increase == True and flag_decrease == False:
+#         return "ASC"
+#     elif flag_increase == False and flag_decrease == True:
+#         return "DESC"
+#     else:
+#         return "MIX"
     
 
-# other learners 
-from datetime import datetime
-def check_order2(the_dates=sys.stdin):
-    # print("Func2")
-    is_asc = is_desc = True
-    dates = [datetime.strptime(line.strip(), '%d.%m.%Y') for line in the_dates]
-    for prev_date, cur_date in zip(dates[:-1], dates[1:]):
-        if prev_date >= cur_date and is_asc:
-            is_asc = False
-        if prev_date <= cur_date and is_desc:
-            is_desc = False
-    if is_asc:
-        return 'ASC'
-    elif is_desc:
-        return 'DESC'
-    else:
-        return 'MIX'
+# # other learners 
+# from datetime import datetime
+# def check_order2(the_dates=sys.stdin):
+#     # print("Func2")
+#     is_asc = is_desc = True
+#     dates = [datetime.strptime(line.strip(), '%d.%m.%Y') for line in the_dates]
+#     for prev_date, cur_date in zip(dates[:-1], dates[1:]):
+#         if prev_date >= cur_date and is_asc:
+#             is_asc = False
+#         if prev_date <= cur_date and is_desc:
+#             is_desc = False
+#     if is_asc:
+#         return 'ASC'
+#     elif is_desc:
+#         return 'DESC'
+#     else:
+#         return 'MIX'
 
 
-# other learners 
-def check_order3(the_dates=sys.stdin):
-    # print("Func3")
-    dates = [datetime.strptime(line.strip(), '%d.%m.%Y') for line in the_dates]
-    if sorted(list(set(dates))) == dates:
-        return('ASC')
-    elif sorted(list(set(dates)), reverse=True) == dates:
-        return('DESC')
-    else:
-        return('MIX')
+# # other learners 
+# def check_order3(the_dates=sys.stdin):
+#     # print("Func3")
+#     dates = [datetime.strptime(line.strip(), '%d.%m.%Y') for line in the_dates]
+#     if sorted(list(set(dates))) == dates:
+#         return('ASC')
+#     elif sorted(list(set(dates)), reverse=True) == dates:
+#         return('DESC')
+#     else:
+#         return('MIX')
 
 
-# other learners 
-from collections import deque
-def check_order4(the_dates=sys.stdin):
-    # print("Func4")
-    dq, record = deque(maxlen=2), set()
-    for dt in the_dates:
-        dq.append(datetime.strptime(dt.strip(), '%d.%m.%Y'))
-        if len(dq) != 2:
-            continue
-        if dq[0] < dq[1]:
-            record.add('ASC')
-        elif dq[0] > dq[1]:
-            record.add('DESC')
-        else:
-            record.add('')
-        if len(record) == 2:
-            return('MIX')
-            break
+# # other learners 
+# from collections import deque
+# def check_order4(the_dates=sys.stdin):
+#     # print("Func4")
+#     dq, record = deque(maxlen=2), set()
+#     for dt in the_dates:
+#         dq.append(datetime.strptime(dt.strip(), '%d.%m.%Y'))
+#         if len(dq) != 2:
+#             continue
+#         if dq[0] < dq[1]:
+#             record.add('ASC')
+#         elif dq[0] > dq[1]:
+#             record.add('DESC')
+#         else:
+#             record.add('')
+#         if len(record) == 2:
+#             return('MIX')
+#             break
 
-    else:
-        return(record)
+#     else:
+#         return(record)
 
 
-def execution_time(func, arg, n):
-    import time
-    t0 = time.monotonic()
-    for _ in range(n):
-        func(arg)
-    t1 = time.monotonic()
-    print(f"{func.__name__:<18} {t1-t0:.2f}")
+# def execution_time(func, arg, n):
+#     import time
+#     t0 = time.monotonic()
+#     for _ in range(n):
+#         func(arg)
+#     t1 = time.monotonic()
+#     print(f"{func.__name__:<18} {t1-t0:.2f}")
+
+
+# # if __name__ == "__main__":
+# #     d = '''14.06.2022
+# # 20.06.2022
+# # 21.06.2022
+# # 17.01.2031
+# # 18.11.2031
+# # 19.11.2031
+# # 23.11.2031
+# # 01.01.2033
+# # 01.11.2043
+# # 05.07.2051
+# # 05.07.2051'''.split('\n')
+# #     print(check_order1(d))
+
 
 
 # if __name__ == "__main__":
+#     funcs = [check_order1, check_order2, check_order3, check_order4]
 #     d = '''14.06.2022
 # 20.06.2022
 # 21.06.2022
@@ -567,22 +584,219 @@ def execution_time(func, arg, n):
 # 01.11.2043
 # 05.07.2051
 # 05.07.2051'''.split('\n')
-#     print(check_order1(d))
+#     for f in funcs:
+#         execution_time(f, d, 100000)
+
+
+
+
+
+######################
+# 4.1.18
+# Гуру прогрессий
+# Дана последовательность целых чисел. Напишите программу, которая 
+# определяет, является ли данная последовательность прогрессией, и 
+# если да, то определяет её вид.
+# Формат входных данных
+# На вход программе подается произвольное количество строк (не менее 
+# трёх), в каждой строке записано натуральное число — очередной член 
+# последовательности.
+# Формат выходных данных
+# Программа должна вывести текст:
+# Арифметическая прогрессия, если введенная последовательность чисел является арифметической прогрессией
+# Геометрическая прогрессия, если введенная последовательность чисел является геометрической прогрессией
+# Не прогрессия, если введенная последовательность чисел не является прогрессией
+
+import sys
+
+def check_sequence(the_numbers=sys.stdin):
+    flag_aryth = True
+    flag_geom = True
+
+    num_0 = float(sys.stdin.readline().strip())
+    num_1 = float(sys.stdin.readline().strip())
+    d_aryth = num_1 - num_0
+    q_geom = num_1 / num_0
+    # aryth = num_1 + d_aryth
+    # geom = num_1 * q_geom
+    aryth = num_1
+    geom = num_1
+
+    # print("num_0", num_0)
+    # print("num_1", num_1)
+    # print("d_aryth", d_aryth)
+    # print("q_geom", q_geom)
+    # print("aryth", aryth)
+    # print("geom", geom)
+
+    for elem in sys.stdin:
+        num = float(elem.strip())
+
+        # print("in loop: num=", num)
+
+        if num != aryth + d_aryth:
+            flag_aryth = False
+        if num != geom * q_geom:
+            flag_geom = False
+        if flag_aryth:
+            aryth += d_aryth
+        if flag_geom:
+            geom *= q_geom
+        
+        # print("in loop: flag_aryth=", flag_aryth)
+        # print("in loop: flag_geom=", flag_geom)
+
+        if flag_aryth == False and flag_geom == False:
+            break
+        # aryth += d_aryth
+        # geom *= q_geom
+
+        # print("in loop: aryth=", aryth)
+        # print("in loop: geom=", geom)
+        # print()
+
+    if not flag_aryth and not flag_geom:
+        return "Не прогрессия"
+    elif flag_aryth:
+        return "Арифметическая прогрессия"
+    else:
+        return "Геометрическая прогрессия"
+
+
+# if __name__ == "__main__":
+#     print(check_sequence())
+
+
+# for testing
+
+
+
+
+#___________
+def is_arithmetic_progression(seq: list) -> bool:
+    for i in range(1, len(seq)-1):
+        if seq[i-1] + seq[i+1] != 2 * seq[i]:
+            return False
+    return True
+
+def is_geometric_progression(seq: list) -> bool:
+    for i in range(1, len(seq)-1):
+        if seq[i-1] * seq[i+1] != seq[i] ** 2:
+            return False
+    return True
+
+def check_sequence2():
+    numbers = list(map(int, sys.stdin))
+    if is_arithmetic_progression(numbers):
+        return "Арифметическая прогрессия"
+    elif is_geometric_progression(numbers):
+        return "Геометрическая прогрессия"
+    else:
+        return "Не прогрессия"
+#___________
+    
+
+
+#___________
+def check_sequence2():
+    n1, n2 = [int(sys.stdin.readline()) for _ in range(2)]
+
+    for num in sys.stdin:
+        if ((int(num) - n2) != (n2 - n1)) and (int(num) / n2) != (n2 / n1):
+            result = 'Не прогрессия'
+            break
+        elif int(num) - n2 == n2 - n1:
+            result = 'Арифметическая прогрессия'
+            n1, n2 = n2, int(num)
+        elif int(num) / n2 == n2 / n1:
+            result = 'Геометрическая прогрессия'
+            n1, n2 = n2, int(num)
+    return (result)
+#___________
+
+
+#___________
+def check_sequence3():
+    from sys import stdin
+
+    nums = [int(n) for n in stdin]
+    nums1 = map(lambda a, b: b - a, nums, nums[1:])
+    nums2 = map(lambda a, b: b / a, nums, nums[1:])
+
+    if len(set(nums1)) == 1:
+        return ('Арифметическая прогрессия')
+    elif len(set(nums2)) == 1:
+        return ('Геометрическая прогрессия')
+    else:
+        return ('Не прогрессия')    
+#___________
+
+
+#___________
+def check_sequence4():
+    current_number = multr = summr = None
+
+    for new_num in sys.stdin:
+        new_num = int(new_num)
+        
+        if current_number is None:
+            current_number = new_num
+            continue
+        if new_num <= current_number:
+            return('Не прогрессия')
+            break
+        if multr is None:
+            multr = new_num / current_number
+            summr = new_num - current_number
+            current_number = new_num
+            continue
+        
+        if summr and new_num - current_number != summr:
+            summr = False
+        if multr and new_num / current_number != multr:
+            multr = False
+        if multr == summr == False:
+            return('Не прогрессия')
+            break
+
+        current_number = new_num
+
+    else:
+        return('Арифметическая' if summr else 'Геометрическая', 'прогрессия')
+
+
+
+# Run tests 
+def execution_time(func, arg, n):
+    import time
+    from io import StringIO
+    t0 = time.monotonic()
+    for _ in range(n):
+        input_io = StringIO(arg)
+        sys.stdin = input_io
+        result = func()
+    t1 = time.monotonic()
+    print(f"{func.__name__:<20} {t1 - t0:.2f}   result: {result}")
 
 
 
 if __name__ == "__main__":
-    funcs = [check_order1, check_order2, check_order3, check_order4]
-    d = '''14.06.2022
-20.06.2022
-21.06.2022
-17.01.2031
-18.11.2031
-19.11.2031
-23.11.2031
-01.01.2033
-01.11.2043
-05.07.2051
-05.07.2051'''.split('\n')
+    # from io import StringIO  # to take control of sys.stdin and send to it test data
+
+    test_data = '''15
+30
+60
+120
+240
+480
+960
+1920
+'''
+   
+    # input_io = StringIO(test_data)
+    # sys.stdin = input_io
+    funcs = [check_sequence, check_sequence2, check_sequence3, check_sequence4]
     for f in funcs:
-        execution_time(f, d, 100000)
+        execution_time(f, test_data, 1000000)
+
+
