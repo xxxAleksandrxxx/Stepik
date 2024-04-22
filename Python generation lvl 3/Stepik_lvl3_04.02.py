5fb53a87ef85f2cb859ba2c830dbd1beccc64375
Aleksandr <sannywind@gmail.com>
2024-04-22 16:13:09 +0700

    add task 4.2.16

--- Python generation lvl 3/Stepik_lvl3_04.02.py
+++ Python generation lvl 3/Stepik_lvl3_04.02.py
@@ -422,3 +422,51 @@
 #         execution_time(func, file, 100000)
 
 
+
+#######################
+# 4.2.16
+# Популярные домены
+# Вам доступен файл data.csv, который содержит неповторяющиеся данные о пользователях некоторого ресурса. В первом столбце записано имя пользователя, во втором — фамилия, в третьем — адрес электронной почты:
+# first_name,surname,email
+# John,Wilson,johnwilson@outlook.com
+# Mary,Wilson,marywilson@list.ru
+# ...
+# Напишите программу, которая создает файл domain_usage.csv, имеющий следующее содержание:
+# domain,count
+# rambler.ru,24
+# iCloud.com,29
+# ...
+# где в первом столбце записано название почтового домена, а во втором — количество пользователей, использующих данный домен. Домены в файле должны быть расположены в порядке возрастания количества их использований, при совпадении количества использований — в лексикографическом порядке.
+# Примечание 1. Разделителем в файле data.csv является запятая, при этом кавычки не используются.
+# Примечание 2. Указанный файл доступен по ссылке. Ответ на задачу доступен по ссылке.
+# Примечание 3. Начальная часть файла domain_usage.csv выглядит так:
+# domain,count
+# rambler.ru,24
+# iCloud.com,29
+# ...
+# Примечание 4. При открытии файла используйте явное указание кодировки UTF-8.
+
+
+import csv
+
+def count_domains(data_file, domains_file):
+    """
+    Reads csv database, counts number of domains, writes new csv file with results
+    """
+    with open(data_file, "r", encoding="utf-8") as f:
+        data = list(csv.DictReader(f, delimiter=","))
+    domain_counter = dict()
+    for record in data:
+        domain = record["email"].split("@")[1]
+        domain_counter[domain] = domain_counter.get(domain, 0) + 1
+    with open(domains_file, "w", encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(["domain", "count"])
+        for k in sorted(domain_counter, key=lambda x: (domain_counter[x], x)):
+            writer.writerow([k, domain_counter[k]])
+        
+
+if __name__ == "__main__":
+    data_file = "etc/data.csv"
+    domains_file = "etc/domain_usage.csv"
+    count_domains(data_file, domains_file)
