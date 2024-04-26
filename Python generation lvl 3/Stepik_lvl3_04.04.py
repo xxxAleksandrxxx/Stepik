e53174d3a5c96c4b08e1054b033ac0f5e01802f0
Aleksandr <sannywind@gmail.com>
2024-04-26 14:47:12 +0700

    add task 4.4.3

--- Python generation lvl 3/Stepik_lvl3_04.04.py
+++ Python generation lvl 3/Stepik_lvl3_04.04.py
@@ -1,34 +1,61 @@
 # #######################
 # 4.4.1
-import json
+# import json
 
-countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
-             'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
-             'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
-             'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+# countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+#              'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+#              'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+#              'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
 
-j_str = json.dumps(obj=countries, sort_keys=True, indent="   ", separators=(",", " - "))
-print(j_str)
+# j_str = json.dumps(obj=countries, sort_keys=True, indent="   ", separators=(",", " - "))
+# print(j_str)
 
 
 # #######################
 # 4.4.2
 # преобразовать словарь words в строку в формате JSON, пропуская пары, которые имеют недопустимые ключи, и присвоить полученный результат переменной data_json
+# import json
+
+# words = {
+#          frozenset(["tap", "telephone"]): ("tæp", "telifəun"),
+#          "travel": "trævl",
+#          ("hello", "world"): ("həˈləʊ", "wɜːld"),
+#          "moonlight": "muːn.laɪt",
+#          "sunshine": "ˈsʌn.ʃaɪn",
+#          ("why", "is", "so", "difficult"): ("waɪ", "ɪz", "səʊ", "ˈdɪfɪkəlt"),
+#          "adventure": "ədˈventʃər",
+#          "beautiful": "ˈbjuːtɪfl",
+#          frozenset(["spoon", "block"]): ("spu:n", "blɔk"),
+#          "bicycle": "baisikl",
+#          ("pilot", "fly"): ("pailət", "flai")
+#         }
+
+# data_json = json.dumps(obj=words, skipkeys=True)
+# print(data_json)
+
+
+# #######################
+# 4.4.3
+# объединить словари club1, club2 и club3 в список и записать полученную структуру данных в файл data.json, указав в качестве отступов три символа пробела.
+
 import json
 
-words = {
-         frozenset(["tap", "telephone"]): ("tæp", "telifəun"),
-         "travel": "trævl",
-         ("hello", "world"): ("həˈləʊ", "wɜːld"),
-         "moonlight": "muːn.laɪt",
-         "sunshine": "ˈsʌn.ʃaɪn",
-         ("why", "is", "so", "difficult"): ("waɪ", "ɪz", "səʊ", "ˈdɪfɪkəlt"),
-         "adventure": "ədˈventʃər",
-         "beautiful": "ˈbjuːtɪfl",
-         frozenset(["spoon", "block"]): ("spu:n", "blɔk"),
-         "bicycle": "baisikl",
-         ("pilot", "fly"): ("pailət", "flai")
-        }
+club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
+         "trainer": "Julian Nagelsmann", "goalkeeper": "M. Neuer", "league_position": 1}
 
-data_json = json.dumps(obj=words, skipkeys=True)
-print(data_json)
+club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
+         "trainer": "Xavier Creus", "goalkeeper": "M. Ter Stegen", "league_position": 7}
+
+club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
+         "trainer": "Michael Carrick", "goalkeeper": "D. De Gea", "league_position": 8}
+
+def combine_dics(file, dics):
+    data_combined = [d for d in dics]
+    # print(data_combined)
+    with open(file, "w", encoding="utf-8") as f:
+        json.dump(obj=data_combined, fp=f, indent="   ")
+
+if __name__ == "__main__":
+    file = "etc/data.json"
+    dics = [club1, club2, club3]
+    combine_dics(file, dics)
