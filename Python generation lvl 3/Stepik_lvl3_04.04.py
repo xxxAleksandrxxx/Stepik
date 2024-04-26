78f92ed8de6c04ce24a421ed6616b9f27474f977
Aleksandr <sannywind@gmail.com>
2024-04-26 14:05:51 +0700

    add task 4.4.1

---
+++ Python generation lvl 3/Stepik_lvl3_04.04.py
@@ -0,0 +1,12 @@
+# #######################
+# 4.4.1
+import json
+
+countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+             'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+             'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+             'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+
+j_str = json.dumps(obj=countries, sort_keys=True, indent="   ", separators=(",", " - "))
+print(j_str)
+
