42a77e8a990ba7bcf03a3184a5d37df2af50bde5
Aleksandr <sannywind@gmail.com>
2024-04-26 14:09:18 +0700

    add task 4.4.2

--- Python generation lvl 3/Stepik_lvl3_04.04.py
+++ Python generation lvl 3/Stepik_lvl3_04.04.py
@@ -10,3 +10,25 @@ countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Ka
 j_str = json.dumps(obj=countries, sort_keys=True, indent="   ", separators=(",", " - "))
 print(j_str)
 
+
+# #######################
+# 4.4.2
+# преобразовать словарь words в строку в формате JSON, пропуская пары, которые имеют недопустимые ключи, и присвоить полученный результат переменной data_json
+import json
+
+words = {
+         frozenset(["tap", "telephone"]): ("tæp", "telifəun"),
+         "travel": "trævl",
+         ("hello", "world"): ("həˈləʊ", "wɜːld"),
+         "moonlight": "muːn.laɪt",
+         "sunshine": "ˈsʌn.ʃaɪn",
+         ("why", "is", "so", "difficult"): ("waɪ", "ɪz", "səʊ", "ˈdɪfɪkəlt"),
+         "adventure": "ədˈventʃər",
+         "beautiful": "ˈbjuːtɪfl",
+         frozenset(["spoon", "block"]): ("spu:n", "blɔk"),
+         "bicycle": "baisikl",
+         ("pilot", "fly"): ("pailət", "flai")
+        }
+
+data_json = json.dumps(obj=words, skipkeys=True)
+print(data_json)
