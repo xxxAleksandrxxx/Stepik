834e99a2db3ab60976bb080f5385256a05396446
Aleksandr <sannywind@gmail.com>
2024-05-05 15:10:24 +0700

    add task 4.5.15

--- Python generation lvl 3/Stepik_lvl3_04.05.py
+++ Python generation lvl 3/Stepik_lvl3_04.05.py
@@ -1,49 +1,67 @@
 # #######################
 # 4.5.14
 # Вам доступен архив workbook.zip, содержащий различные папки и файлы. Напишите программу, которая выводит единственное число — количество файлов в этом архиве.
-from zipfile import ZipFile
+# from zipfile import ZipFile
 
-def count_files1(file_in):
-    with ZipFile(file_in, 'r') as zf:
-        info = zf.namelist()
-    count = 0
-    for elem in info:
-        if elem.endswith("/"):
-            continue
-        else:
-            count += 1
-    # print(count)
-
-
-def count_files2(file_in):
-    with ZipFile(file_in, 'r') as zf:
-        a = sum(not elem.is_dir() for elem in zf.infolist())
-        # print(a)
+# def count_files1(file_in):
+#     with ZipFile(file_in, 'r') as zf:
+#         info = zf.namelist()
+#     count = 0
+#     for elem in info:
+#         if elem.endswith("/"):
+#             continue
+#         else:
+#             count += 1
+#     # print(count)
 
 
-def count_files3(file_in):
-    with ZipFile(file_in, 'r') as zf:
-        count = 0
-        for elem in zf.infolist():
-            if not elem.is_dir():
-                count += 1
-    # print(count)
+# def count_files2(file_in):
+#     with ZipFile(file_in, 'r') as zf:
+#         a = sum(not elem.is_dir() for elem in zf.infolist())
+#         # print(a)
 
 
-def execution_time(func, *args, n=10000):
-    from time import monotonic
-    t0 = monotonic()
-    for _ in range(n):
-        func(*args)
-    t1 = monotonic()
-    print(f"{func.__name__:<15} {t1-t0:.2f}")
+# def count_files3(file_in):
+#     with ZipFile(file_in, 'r') as zf:
+#         count = 0
+#         for elem in zf.infolist():
+#             if not elem.is_dir():
+#                 count += 1
+#     # print(count)
 
-if __name__ == "__main__":
-    file_in = "etc/workbook.zip"
-    # count_files2(file_in)
 
-    funcs = [count_files1, count_files2, count_files3]
-    for func in funcs:
-        execution_time(func, file_in, n=10**5)
+# def execution_time(func, *args, n=10000):
+#     from time import monotonic
+#     t0 = monotonic()
+#     for _ in range(n):
+#         func(*args)
+#     t1 = monotonic()
+#     print(f"{func.__name__:<15} {t1-t0:.2f}")
+
+# if __name__ == "__main__":
+#     file_in = "etc/workbook.zip"
+#     # count_files2(file_in)
+
+#     funcs = [count_files1, count_files2, count_files3]
+#     for func in funcs:
+#         execution_time(func, file_in, n=10**5)
     
 
+
+# #######################
+# 4.5.15
+# Вам доступен архив workbook.zip, содержащий различные папки и файлы. Напишите программу, которая выводит суммарный объем файлов этого архива в сжатом и не сжатом видах в байтах,
+
+from zipfile import ZipFile
+
+def print_size_info(file_in):
+    with ZipFile(file_in, "r") as zf:
+        info = zf.infolist()
+    size_decompressed = sum(elem.file_size for elem in info)
+    size_compressed = sum(elem.compress_size for elem in info)
+    print("Объем исходных файлов:", size_decompressed, "байт(а)")
+    print("Объем сжатых файлов:", size_compressed, "байт(а)")
+
+if __name__ == "__main__":
+    file_in = 'etc/workbook.zip'
+    print_size_info(file_in)
