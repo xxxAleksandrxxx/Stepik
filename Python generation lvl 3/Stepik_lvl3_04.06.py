7d4ae8ce23b9b5477a878c9b52b3aee3da37e448
Aleksandr <sannywind@gmail.com>
2024-05-06 16:09:50 +0700

    add task 4.6.16

--- Python generation lvl 3/Stepik_lvl3_04.06.py
+++ Python generation lvl 3/Stepik_lvl3_04.06.py
@@ -3,16 +3,38 @@
 # Одинокая функция
 # Дан pickle файл, содержащий единственную сериализованную функцию. Напишите программу, которая вызывает данную функцию с заданными аргументами и выводит возвращаемое значение функции.
 
+# import pickle
+# import sys
+
+# def run_pickle_func1(pickle_func, args:list):
+#     with open(pickle_func, "rb") as pkf:
+#         func = pickle.load(pkf)
+#         print(func(*args))
+
+
+# def run_pickle_func2(pickle_func, args:list):
+#     with open(pickle_func, "rb") as pkf:
+#         print(pickle.load(pkf)(*args))
+
+# if __name__ == "__main__":
+#     pickle_func, *args = [line.strip() for line in sys.stdin]
+#     run_pickle_func2(pickle_func, args)
+
+
+# #######################
+# 4.6.16
+# Ты не пройдешь
+# Реализуйте функцию filter_dump(), которая принимает три аргумента в следующем порядке:
+# filename — название pickle файла, например, data.pkl
+# objects — список произвольных объектов
+# typename — тип данных
+# Функция должна создавать pickle файл с названием filename, который содержит сериализованный список только тех объектов из списка objects, тип которых равен typename.
+
 import pickle
-import sys
 
-def run_pickle_func(pickle_func, args:list):
-    with open(pickle_func, "rb") as pkf:
-        func = pickle.load(pkf)
-        print(func(*args))
-
-if __name__ == "__main__":
-    pickle_func, *args = [line.strip() for line in sys.stdin]
-    run_pickle_func(pickle_func, args)
+def filter_dump(f_name, obj_in, type_name):
+    with open(f_name, "wb") as f_pkl:
+        obj_pkl = [elem for elem in obj_in if type(elem) == type_name]
+        pickle.dump(obj=obj_pkl, file=f_pkl)
 
 
